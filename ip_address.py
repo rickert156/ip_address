d6ae4f4fadc6745a3fa595b3a062fa4a03eb9c8a
rickert <rickert156@rambler.ru>
2024-04-12 16:15:02 +0400

    firs commit

---
+++ ip_address.py
@@ -0,0 +1,25 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import requests
+import time
+
+ip_local = '' #Подставить  свой IP
+
+def ipAddress():
+	try:
+		site = urlopen('https://api.ipify.org/')
+
+		bs = BeautifulSoup(site, 'lxml')
+		try:
+			if bs.body.get_text() == ip_local:
+				print(bs.body.get_text(),'Настоящий IP')
+				# Сработает, если ip_local был указан заранее
+			else:
+				print(bs.body.get_text())
+		except:
+			print('Ошибка в обработке исключений')
+	except:
+		print("Error")
+while True:
+	ipAddress()
+	time.sleep(1)
